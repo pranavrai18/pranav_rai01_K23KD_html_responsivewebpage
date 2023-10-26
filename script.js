1c4d2a0d3b7112fe4a185df9a8c2a77c022dd765
raisahab09 <140087392+raisahab09@users.noreply.github.com>
2023-10-27 02:34:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// var videocon = document.querySelector("#video container")
+// var playbtn = document.querySelector("#play")
+// videocon.addEventListener("mouseenter",function(){
+//     playbtn.style.opacity = 1
+//     playbtn.style.scale = 1
+// })
